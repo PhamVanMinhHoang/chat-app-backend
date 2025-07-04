960ee5a29fc74dd70ac6054fa3eaa0147387f37d
Lucifer199x <phamvanminhhoang2@gmail.com>
2025-07-04 17:41:09 +0700

    .

--- src/routes/api.php
+++ src/routes/api.php
@@ -6,3 +6,16 @@ use Illuminate\Support\Facades\Route;
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:api');
+
+Route::group([
+    'prefix' => 'v1',
+], function () {
+    Route::group([
+        'prefix' => 'auth',
+    ], function () {
+        Route::post('/login', 'AuthController@login');
+        Route::post('/register', 'AuthController@register');
+
+    });
+});
+
